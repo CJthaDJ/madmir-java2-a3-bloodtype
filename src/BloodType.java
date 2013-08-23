6894e15f67708e41571084b26f3a6225642291b6
CJ Bernard <cjthadj@hotmail.ca>
2013-08-23 11:53:12 -0400

    Here you go Clyde, Have a great weekend.

--- src/BloodType.java
+++ src/BloodType.java
@@ -1,4 +1,45 @@
 
 public class BloodType {
 
+	private String bloodType;
+	private char rhFactor;
+		
+	//Constructors
+	public BloodType(String newBloodType, char newRhFactor) {
+		bloodType = newBloodType;
+		rhFactor = newRhFactor;
+	}
+
+	public BloodType(String newBloodType) {
+		this(newBloodType, '+');
 }
+
+	public BloodType(char newRhFactor) {
+		this("0", newRhFactor);
+	}
+	
+	public BloodType() {
+		this("0", '+');
+	}
+	
+	
+	//Accessor methods
+	public String getBloodType() {
+		return bloodType;
+	}
+	
+	public char getRhFactor() {
+		return rhFactor;
+	}
+	
+	//Mutator methods
+	public void setBloodType(String blood) {
+		bloodType = blood;
+	}
+	
+	public void setRhFactor(char rh) {
+		rhFactor = rh;
+	}
+	
+}
+
--- src/TestPatient.java
+++ src/TestPatient.java
@@ -1,7 +1,31 @@
 
 public class TestPatient {
-
+    //Enter Patient ID, Blood Type, Age and RH Factor
 	public static void main(String[] args) {
+		BloodType ashleyBloodType = new BloodType("B", '-');
+		Patient ashley = new Patient(86753, 21, ashleyBloodType);
+		BloodType keithBloodType = new BloodType("A", '+');
+		Patient keith = new Patient(86800, 35, keithBloodType);
+		BloodType clydeBloodType = new BloodType("AB", '-');
+		Patient clyde = new Patient(88000, 37, clydeBloodType);
+		
+		
+	//Enter Patients Age and ID Number	
+		ashley.setPatientAge(21);
+		ashley.setPatientsIdNumber(10010);
+		
+	// User Output	
+		System.out.println("Ashley's ID number is  " + ashley.getPatientIdNumber() + " She is  +"
+				+ ashley.getPatientAge() + " years old, Her blood type is " + ashley.getBloodType());
+		
+		System.out.println("Keith's ID number is " + keith.getPatientIdNumber() + " He is +"
+				+ keith.getPatientAge() + " years old,  His blood type is  " + keith.getBloodType());
+		
+		System.out.println("Clyde's ID number is  " + clyde.getPatientIdNumber() + " He is   +"
+				+ clyde.getPatientAge() + "  years old, His blood type is  " + clyde.getBloodType());
+		
+		
+		
 		
 
 	}
